a4ce3d58a9cb1a7fab64cd22cf7696798726063f
Obiageli Ogbutor <obygirl81@gmail.com>
2019-11-11 20:40:37 -0700

    uploading completed files

--- models/Note.js
+++ models/Note.js
@@ -1,21 +1,29 @@
+// Note model
+// ==========
+
 // Require mongoose
 var mongoose = require("mongoose");
-// Create a schema class
+// Create the schema class using mongoose's schema method
 var Schema = mongoose.Schema;
 
-// Create the Note schema
-var NoteSchema = new Schema({
-    body: {
-        type: String
-    },
-    article: {
-        type: Schema.Types.ObjectId,
-        ref: "Article"
-    }
+// Create the noteSchema with the schema object
+var noteSchema = new Schema({
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // as is the noteText
+  noteText: String
 });
 
-// Create the Note model with the NoteSchema
-var Note = mongoose.model("Note", NoteSchema);
+// Create the Note model using the noteSchema
+var Note = mongoose.model("Note", noteSchema);
 
 // Export the Note model
 module.exports = Note;
